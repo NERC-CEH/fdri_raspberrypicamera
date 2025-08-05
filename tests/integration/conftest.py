1f7f31e8cf1690a54bc88dbec987f3a37e7b9f2c
Jo Walsh <metazool@fastmail.net>
2025-08-05 11:52:07 +0100

    type hints for ruff

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,8 +1,10 @@
-import pytest
 from pathlib import Path
+from typing import Any
+
+import pytest
 
 
-def pytest_addoption(parser):
+def pytest_addoption(parser: Any) -> None:
     """Configurable default for device serial # when running hardware tests"""
     parser.addoption("--endpoint", action="store", default="")
     parser.addoption("--device_id", action="store", default="00000")
@@ -10,31 +12,35 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture
-def fixture_dir():
+def fixture_dir() -> Path:
     return Path(__file__).parent.parent.parent.resolve()
 
 
 @pytest.fixture()
-def endpoint(pytestconfig):
+def endpoint(pytestconfig: Any) -> str:
     return pytestconfig.getoption("endpoint")
 
+
 @pytest.fixture()
-def alias(pytestconfig):
+def alias(pytestconfig: Any) -> str:
     return pytestconfig.getoption("alias")
 
+
 @pytest.fixture()
-def device_id(pytestconfig):
+def device_id(pytestconfig: Any) -> str:
     return pytestconfig.getoption("device_id")
 
-@pytest.fixture()
-def cert_file(fixture_dir, device_id):
-    return fixture_dir / f'{device_id}-certificate.pem.crt'
 
 @pytest.fixture()
-def key_file(fixture_dir, device_id):
-    return fixture_dir / f'{device_id}-private.pem.key'
+def cert_file(fixture_dir: Path, device_id: str) -> Path:
+    return fixture_dir / f"{device_id}-certificate.pem.crt"
+
 
 @pytest.fixture()
-def cert_root(fixture_dir):
-    return fixture_dir / 'CARoot.pem'
+def key_file(fixture_dir: Path, device_id: str) -> Path:
+    return fixture_dir / f"{device_id}-private.pem.key"
 
+
+@pytest.fixture()
+def cert_root(fixture_dir: Path, device_id: str) -> Path:
+    return fixture_dir / "CARoot.pem"
